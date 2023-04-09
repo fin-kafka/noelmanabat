35a741dcbdb8d163cc9bde7f0b38625185191bf5
mananoel <noel.manabat35@gmail.com>
2023-04-09 02:46:45 +0200

    export doc

--- shared/components/index.ts
+++
@@ -1,9 +0,0 @@
-import Navbar from './navbar';
-import Loader from './loader';
-import SocialBar from './socialbar';
-import Button from './buttons';
-import HeaderSmall from './header-small';
-import Footer from './footer';
-import ProjectCard from './project-card';
-
-export { Navbar, SocialBar, Loader, Button, HeaderSmall, Footer, ProjectCard };
